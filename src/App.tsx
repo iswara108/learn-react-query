c55032b93ed5534d7c2af7866ba46b836c171b7c
iswara.chaitanya <iswara@sivananda.org>
2021-12-16 16:39:23 -0500

    basic static query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { useQuery } from 'react-query'
 
 function App() {
-  return <div>My App</div>
+  const queryInfo = useQuery<{ name: string; url: string }[]>('pokemon', () =>
+    new Promise(res => setTimeout(res, 2000))
+      .then(() => fetch('https://pokeapi.co/api/v2/pokemon'))
+      .then(res => res.json())
+      .then(res => res.results)
+  )
+
+  console.log(queryInfo)
+
+  return queryInfo.isLoading ? (
+    <>Loading...</>
+  ) : queryInfo.isError ? (
+    <>Error</>
+  ) : queryInfo.isIdle ? (
+    <>Idle</>
+  ) : (
+    <div>
+      {queryInfo.data.map(result => (
+        <div key={result.name}>{result.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export default App
