e7facb31a39ff56d0d57e701760091079b1b67ec
iswara.chaitanya <iswara@sivananda.org>
2021-12-16 16:13:38 -0500

    fresh start

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,7 @@
-import React from "react";
-import "./App.css";
-
-import {
-  // useQuery,
-  // useMutation,
-  // useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-  useInfiniteQuery,
-} from "react-query";
-import { getProjects, getTodos, postTodo } from "./my-api";
-import { ReactQueryDevtools } from "react-query/devtools";
-
-// Create a client
-const queryClient = new QueryClient();
+import React from 'react'
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Projects />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <div>My App</div>
 }
 
-function Projects() {
-  const fetchProjects = ({ pageParam = 0 }) => getProjects(pageParam);
-
-  const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    status,
-    refetch,
-  } = useInfiniteQuery("projects", fetchProjects, {
-    getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
-  });
-
-  return status === "loading" ? (
-    <p>Loading...</p>
-  ) : status === "error" && error instanceof Error ? (
-    <p>Error: {error.message}</p>
-  ) : (
-    <>
-      <button onClick={() => refetch()}>refetch</button>
-      {data?.pages.map((group, i) => (
-        <React.Fragment key={i}>
-          {group.projects.map((project) => (
-            <p key={project.id}>{project.name}</p>
-          ))}
-        </React.Fragment>
-      ))}
-      <div>
-        <button
-          onClick={() => fetchNextPage()}
-          disabled={!hasNextPage || isFetchingNextPage}
-        >
-          {isFetchingNextPage
-            ? "Loading more..."
-            : hasNextPage
-            ? "Load More"
-            : "Nothing more to load"}
-        </button>
-      </div>
-      <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
-    </>
-  );
-}
-
-export default App;
+export default App
